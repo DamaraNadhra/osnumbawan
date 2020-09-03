6f7f5882a337c77c0c6a0177d244b334bc5b3ee3
DamaraNadhra <70007198+DamaraNadhra@users.noreply.github.com>
2020-09-03 18:28:45 +0700

    Update os.js

--- os.js
+++ os.js
@@ -6,7 +6,7 @@ const ms = require('ms');
 
 const prefix = '&'
 
-client.on('message', async (message) => {
+client.on('message', (message) => {
 
     const args = message.content.slice(prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
