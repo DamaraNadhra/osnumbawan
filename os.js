253fe2d0f789a44c284a288797836031d08c63ee
DamaraNadhra <70007198+DamaraNadhra@users.noreply.github.com>
2020-09-08 12:34:10 +0700

    Update os.js

--- os.js
+++ os.js
@@ -978,6 +978,65 @@ let shamanHighlord  = message.guild.roles.cache.find(lord => lord.id === '712346
         }
         })
         message.channel.send(`Giveaway starting in ${channel}`);
+    } else
+    if (command === 'reroll') {
+        if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("You don't have permission to reroll a giveaways");
+
+        if (!args[0]) return message.channel.send("No giveaway ID provided");
+
+        let giveaway = client.giveawayManager.giveaways.find((g) => g.prize === args.join(" ")) || client.giveawayManager.giveaways.find((g) => g.messageID === args[0]);
+
+        if (!giveaway) return message.channel.send("Cannot find an giveaway with that ID!");
+
+        client.giveawayManager.reroll(giveaway.messageID)
+        .then(() => {
+            message.channel.send('Giveaway rerolled')
+        }).catch((e) => {
+            if (e.startsWith(`Giveaway with ID ${giveaway.messageID} is not ended`)) {
+                message.channel.send("This giveaway hasn't ended yet") 
+            } else {
+                console.error(e);
+                message.channel.send('An error occured')
+            }
+        })
+    } else
+    if (command === 'end') {
+        if(!message.member.hasPermission('MANAGE_MESSAGES') && !message.member.roles.cache.some((r) => r.name === "Giveaways")){
+            return message.channel.send(':x: You need to have the manage messages permissions to end giveaways.');
+        }
+        if(!args[0]){
+            return message.channel.send(':x: You have to specify a valid message ID!');
+        }
+    
+
+        let giveaway = 
+ 
+        client.giveawayManager.giveaways.find((g) => g.prize === args.join(' ')) ||
+   
+        client.giveawayManager.giveaways.find((g) => g.messageID === args[0]);
+    
+
+        if(!giveaway){
+            return message.channel.send('Unable to find a giveaway for `'+ args.join(' ') + '`.');
+        }
+    
+
+        client.giveawayManager.edit(giveaway.messageID, {
+            setEndTimestamp: Date.now()
+        })
+
+        .then(() => {
+
+            message.channel.send('Giveaway will end in less than '+(client.giveawayManager.options.updateCountdownEvery/1000)+' seconds...');
+        })
+        .catch((e) => {
+            if(e.startsWith(`Giveaway with message ID ${giveaway.messageID} is already ended.`)){
+                message.channel.send('This giveaway is already ended!');
+            } else {
+                console.error(e);
+                message.channel.send('An error occured...');
+            }
+        });
     }
 
 })
